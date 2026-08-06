d96f986c648c5c2341972b216ead868d1e040d05
KavyaAgarwal-117 <kavyaagarwal1708@gmail.com>
2026-08-06 12:27:58 +0530

    UserRouter.js

--- models/UserModels.js
+++ models/UserModels.js
@@ -4,6 +4,7 @@ const mySchema = new Schema({
     name : { type : String, require : true},
     email : { type : String, unique : true},
     password : { type : String, require : true},
+    city: { type: String, default: 'Unknown'}
 }, { timestamps: true });
 
 module.exports = model('users', mySchema);
--- routers/UserRouter.js
+++ routers/UserRouter.js
@@ -25,6 +25,28 @@ router.get('/getall', (req, res) => {
     });
 });
 
+// : denotes url parameter
+router.get('/getbycity/:city', (req,res) => {
+    Model.find({city: req.params.city})
+    .then((result) => {
+        res.status(200).json(result);
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+})
+
+router.get('/getbyemail/:email', (req,res) => {
+    Model.find({email: req.params.email})
+    .then((result) => {
+        res.status(200).json(result);
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+})
+
+
 router.get('/getbyid', (req, res) => {
     res.send('response from user getbyid');
 });
