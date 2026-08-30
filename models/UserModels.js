1c31a2097deff2f5d8e379ffd2d8abaf513767fb
KavyaAgarwal-117 <kavyaagarwal1708@gmail.com>
2026-08-30 19:04:12 +0530

    User Model

--- models/UserModels.js
+++ models/UserModels.js
@@ -1,10 +1,13 @@
-const { Schema, model } = require("mongoose")
+const { Schema, model } = require("mongoose");
 
-const mySchema = new Schema({
-    name : { type : String, require : true},
-    email : { type : String, unique : true},
-    password : { type : String, require : true},
-    city: { type: String, default: 'Unknown'}
-}, { timestamps: true });
+const mySchema = new Schema(
+  {
+    name: { type: String, require: true },
+    email: { type: String, unique: true },
+    password: { type: String, require: true },
+    city: { type: String, default: "Unknown" },
+  },
+  { timestamps: true },
+);
 
-module.exports = model('users', mySchema);
+module.exports = model("users", mySchema);
